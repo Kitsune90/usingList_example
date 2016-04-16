fd8dda222239d0615a673617626546254aaa66ce
Kitsune90 <cristianocavo@icloud.com>
2016-04-16 15:24:58 +0200

    Initial Commit

---
+++ usingList_example/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  usingList_example
+//
+//  Created by Cristiano Cavo on 16/04/16.
+//  Copyright © 2016 Cristiano Cavo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
