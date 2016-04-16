a6f3f39712c077abee65960241190418d21625dd
Kitsune90 <cristianocavo@icloud.com>
2016-04-16 15:57:52 +0200

    Initial commit

--- usingList_example/main.cpp
+++ usingList_example/main.cpp
@@ -7,9 +7,32 @@
 //
 
 #include <iostream>
+#include <list>
+#include <iterator>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    std::list<std::string> miaLista;
+    
+    std::string str1 = "asd";
+    std::string str2 = "fgh";
+    std::string str3 = "vbn";
+    std::string str4 = "zxc";
+    std::string str5 = "qwe";
+    
+    miaLista.push_back(str1);
+    miaLista.push_back(str2);
+    miaLista.push_back(str3);
+    miaLista.push_back(str4);
+    miaLista.push_back(str5);
+    
+    for (std::list<std::string>::iterator iterator = miaLista.begin(),
+         end = miaLista.end();
+         iterator != end;
+         ++iterator)
+    {
+        std::cout << *iterator << std::endl;
+    }
+    
     return 0;
 }
